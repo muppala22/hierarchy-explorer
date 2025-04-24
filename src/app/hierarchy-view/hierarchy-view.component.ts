e41a19d1742a5c15819dea600edd719fad228999
Meenakshi Uppala <meenauppala@gmail.com>
2025-04-23 19:56:04 -0700

    hierarchy view files

--- src/app/hierarchy-view/hierarchy-view.component.ts
+++ src/app/hierarchy-view/hierarchy-view.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { HierarchyNode } from '../models/hierarchy-model.module';
+import { FormsModule } from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {HierarchyNodeComponent} from '../hierarchy-node/hierarchy-node.component';
 
 @Component({
   selector: 'app-hierarchy-view',
-  imports: [],
   templateUrl: './hierarchy-view.component.html',
-  styleUrl: './hierarchy-view.component.css'
+  imports: [
+    FormsModule,
+    NgForOf,
+    HierarchyNodeComponent
+  ],
+  styleUrls: ['./hierarchy-view.component.scss']
 })
 export class HierarchyViewComponent {
-
+  hierarchy: HierarchyNode[] = [
+    {
+      id: '1',
+      label: 'Global | BG',
+      type: 'G',
+      expanded: true,
+      children: [
+        {
+          id: '2',
+          label: 'Country: Bulgaria',
+          type: 'C',
+          expanded: false,
+          children: [
+            {
+              id: '3',
+              label: 'Facility: Sofia',
+              type: 'F',
+              children: [
+                {
+                  id: '4',
+                  label: 'EAN: 1234567890123',
+                  type: 'EAN'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
 }
