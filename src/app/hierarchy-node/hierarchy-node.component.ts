0cf19895c2c3afa5e20d6ff5a5180e6020f15cdc
Meenakshi Uppala <meenauppala@gmail.com>
2025-04-24 09:30:28 -0700

    all uncommitted files

---
+++ src/app/hierarchy-node/hierarchy-node.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { HierarchyNode } from '../models/hierarchy-model.module';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-hierarchy-node',
+  templateUrl: './hierarchy-node.component.html',
+  imports: [
+    CommonModule
+  ],
+  styleUrls: ['./hierarchy-node.component.scss']
+})
+export class HierarchyNodeComponent {
+  @Input() node!: HierarchyNode;
+
+  toggle() {
+    this.node.expanded = !this.node.expanded;
+  }
+}
